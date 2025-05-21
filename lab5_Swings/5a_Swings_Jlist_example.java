27c799c0878fab059ca46a1aa38840d9a2fb13f6
Roshan474 <roshu176@gmail.com>
2025-05-21 15:14:49 +0530

    5a_Swings_Jlist_example.java

--- lab5_Swings/5a_Swings_Jlist_example.java
+++ lab5_Swings/5a_Swings_Jlist_example.java
@@ -1,5 +1,5 @@
 /*
-Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada,
+5A: Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada,
 Denmark, France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and
 display them on console whenever the countries are selected on the list.
 */
